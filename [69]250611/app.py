e3a29c8f8e19595bf23f2b5ff85de804b4245040
jmlee <jeongmin0507@naver.com>
2025-06-11 17:12:38 +0900

    0611_aft04

--- [69]250611/app.py
+++ [69]250611/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 
 # 객체 생성
 app = Flask(__name__)
+app.secret_key = 'secret key'
 
 @app.route('/')
 def index():
@@ -18,8 +19,17 @@ def projects():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        return render_template('contact.html', success=True)
-    return render_template('contact.html', success=False)
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        print(f'제출된 데이터 이름: {name} 이메일: {email} 메시지: {message}')
+        # 리다이렉트 success 표시
+        session['success'] = True
+        return redirect(url_for('contact')) # get 방식
+    
+    # get요청 시 success 표시 후 초기화
+    success = session.pop('success', False)
+    return render_template('contact.html', success=success)
 
 if __name__ == '__main__':
     app.run(debug=True)
