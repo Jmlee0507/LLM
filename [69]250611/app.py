6d5d74d0eef29058a2d4c877ef3138017748ef3c
jmlee <jeongmin0507@naver.com>
2025-06-11 15:48:55 +0900

    0611_aft03

---
+++ [69]250611/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+# 객체 생성
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', success=True)
+    return render_template('contact.html', success=False)
+
+if __name__ == '__main__':
+    app.run(debug=True)
