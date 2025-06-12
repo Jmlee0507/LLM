0e02e57364422d54cbdbc17c1e717d99b7a5c287
jmlee <jeongmin0507@naver.com>
2025-06-12 13:45:32 +0900

    0612_morn4

---
+++ [70]250612/api/backend/schemas.py
@@ -0,0 +1,37 @@
+# 요청 / 응답 모델 정의
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+
+    # ORM 객체를 직렬화할 수 있도록 ==> DB에서 가져온 객체를 
+    class Config:
+        orm_mode = True
+
+#상품 등록 데이터객체
+class ProductCreate(BaseModel):
+    name: str
+    price: int
+
+class ProductOut(BaseModel):
+    name: str
+    price: int
+    class Config: # 객체로 리턴할 때
+        orm_mode = True
+
+class CartItem(BaseModel):
+    user_id: int
+    product_id: int
+    quantity: int
+class OrderOut(BaseModel):
+    id: int
+    user_id: int    
+    product_id: int
+    quantity: int
+    class Config:   # 객체로 리턴할때
+        orm_mode = True     
