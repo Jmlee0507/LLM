66482ed044d9ca3631c869dbfd36f204fab5270e
jmlee <jeongmin0507@naver.com>
2025-06-17 10:49:49 +0900

    Feat: migrate board

--- [73]250617/README.md
+++ [73]250617/README.md
@@ -1,1 +1,7 @@
-# 장고
+# Migration
+```
+model이 새로 생성되거나 변경된 경우 DB를 갱신해야 하는데,
+makemigration -> migration 해서 반영을 한다
+
+처음에 DB만 생성했기 때문에 migration 해야 기본 테이블들이 만들어진다
+```
--- [73]250617/board/models.py
+++ [73]250617/board/models.py
@@ -11,7 +11,10 @@ class Question(models.Model):
         return self.subject # 객체를 문자열로 표현할 때 제목을 반환
 
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE) #답변 제목
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # 질문과의 관계 설정
     content = models.TextField() # 답변 내용
     created_at = models.DateTimeField(auto_now_add = True) # 생성 시간
-    updated_at = models.DateTimeField(auto_now = True) # 수정 시간  
+    updated_at = models.DateTimeField(auto_now = True) # 수정 시간
+
+    def __str__(self):
+        return f"Answer to {self.question.subject}"  # 객체를 문자열로 표현할 때 질문 제목을 포함한 답변을 반환
