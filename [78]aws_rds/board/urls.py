1d4f5c73e28be229e124f7392ff31cfb555b84aa
jmlee <jeongmin0507@naver.com>
2025-06-24 15:14:43 +0900

    0624_aft2

---
+++ [78]aws_rds/board/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import *
+
+
+urlpatterns = [
+    path('new/', board_new, name='board_lists'), # create
+    path('', board_lists, name='board_lists'), # read
+    path('update/<int:pk>', board_update, name='board_update'), # update
+    path('delete/<int:pk>', board_delete, name='board_delete'), # delete
+]
---
+++ [78]aws_rds/board/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import *
+
+def board_lists(request):
+    post = Post.objects.order_by('created_at') #내림차순
+    return render(request, 'board/lists.html', {'posts':post})
+
+def board_new(request):
+    if request.method == 'POST':
+        Post.objects.create(title = request.POST['title'],
+                            content = request.POST['content'])
+        return redirect('board_lists')
+    return render(request, 'board/new.html')
+
+def board_update(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    if request.method == 'POST':
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.save()
+        return redirect('board_lists')
+    return render(request, 'board/update.html', {'post':post})
+
+def board_delete(request,pk):
+    post = get_object_or_404(Post, id=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('board_lists')
+    return render(request, 'board/delete.html',{'post':post})
